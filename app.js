42bb76e0fa1bb4ad574c197c40b70212c9e004e6
Beanxx <tnqls991211@naver.com>
2022-06-26 21:27:26 +0900

    feat: server CRUD api

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // TODO: cors를 적용합니다.
+app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
+app.use(morgan("tiny"));
 
 const port = 3001;
-const discussionsRouter = require('./router/discussions'); 
-// TODO: /discussions 경로로 라우팅합니다. 
-// 라우팅 앞에 주소 를 쳤을경우 뒤에 오는 값의 있는 애들을 참조해라
+const diaryRouter = require("./router/diaryRouter");
+// TODO: /discussions 경로로 라우팅합니다.
+app.use("/diary", diaryRouter);
 
-  // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+// TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+app.get("/", (req, res) => {
+  res.status(200).send("열려라 참깨!~!");
+});
 
-
+app.use((req, res, next) => {
+  res.status(404).send("Not Found!");
+});
 
 const server = app.listen(port, () => {
-  console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
+  console.log(`[RUN] My CRUD diary Server... | http://localhost:${port}`);
 });
 
 module.exports.app = app;
 module.exports.server = server;
- 
--- controller/index.js
+++ controller/index.js
@@ -1,29 +1,50 @@
-const { agoraStatesDiscussions } = require("../repository/discussions");
-const discussionsData = agoraStatesDiscussions;
-const discussionsController = {
+const { diaryDummyData } = require("../repository/diaryData");
+const diaryData = diaryDummyData;
+const diaryController = {
+  // router.get("/", findAll);
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    return res.status(200).json(diaryData);
   },
-    
-  findById: (req, res) => { 
-  
+
+  // router.get("/:id", findById);
+  findById: (req, res) => {
+    if (req.params) {
+      const idx = diaryData.find((item) => item.id === Number(req.params.id));
+      // 조회하려는 id가 있다면
+      if (idx !== undefined) {
+        res.status(200).json(idx);
+      }
+    }
+    res.status(404).send("Not found!!");
   },
   createOne: (req, res) => {
+    const newItem = req.body;
+    diaryData.unshift(newItem);
 
+    return res.status(200).send("Create Diary!!");
   },
 
   updateById: (req, res) => {
+    const updated = diaryData.filter((item) => item.id === Number(req.body.id));
+    const change = { ...updated[0], ...req.body };
+    const idx = diaryData.findIndex(
+      (item) => item.id === Number(req.params.id)
+    );
 
+    diaryData.splice(idx, 1, change);
+    return res.status(200).json(change);
   },
 
   deleteById: (req, res) => {
-
+    const deleted = diaryData.findIndex(
+      (item) => item.id === Number(req.params.id)
+    );
+    diaryData.splice(deleted, 1);
+    return res.status(200).json();
   },
-}; 
-
-
-module.exports = {
-  discussionsController,
 };
 
-
+module.exports = {
+  diaryController,
+};
